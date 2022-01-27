08182cb8ed281393fb71717e388be83e71c306a3
Harsha <77527524+harsha-deep@users.noreply.github.com>
2022-01-27 10:04:28 +0530

    Delete tests directory

--- tests/driver.py
+++
@@ -1,5 +0,0 @@
-import sponge
-
-if __name__ == "__main__":
-    n = int(input())
-    print(sponge.func(n))
--- tests/sponge.py
+++
@@ -1,6 +0,0 @@
-def func(n):
-    for i in range(10):
-        if n == i:
-            return True
-        else:
-            return False
